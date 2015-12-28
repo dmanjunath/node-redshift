a9e732999462dc6ece5a3c30ee0fbb323acb83af
Dheeraj Manjunath <dheerajmanju1@yahoo.com>
2015-12-27 20:15:11 -0500

    creating models through CLI, can add record through create() function

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 module.exports = require('./lib/connection.js');
+module.exports.setConfig = require('./lib/connection.js').setConfig;
 module.exports.query = require('./lib/query.js');
+module.exports.model = require('./lib/model.js');
+module.exports.import = require('./lib/model.js').import;
