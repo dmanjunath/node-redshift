ee957609073c40e2a693b424f9940d86dec40e6c
Dheeraj Manjunath <dheerajmanju1@yahoo.com>
2017-01-09 00:06:45 -0500

    Dont allow nested connection requests

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 module.exports = require('./lib/connection.js');
-module.exports.setConfig = require('./lib/connection.js').setConfig;
 module.exports.query = require('./lib/query.js');
 module.exports.model = require('./lib/model.js');
 module.exports.import = require('./lib/model.js').import;
